fbe551abf91a41d5d2fb49d4ff2e3e0ae66350f0
Devanshu Agarwal (agdeva8) <agarwaldevanshu8@gmail.com>
2020-05-15 17:12:02 +0530

    All Players responds to click on move accordingly

--- Assets/Scripts/Corourines/MovePlayer.cs
+++ Assets/Scripts/Corourines/MovePlayer.cs
@@ -1,9 +1,13 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.EventSystems;
 
 public static class MovePlayer
 {
+    public static GameObject Player;
     public static void Start()
     {
     }
@@ -15,17 +19,27 @@ public static class MovePlayer
         int currStep = 0;
         while (currStep < numMoves)
         {
-            if (!MovePlayerSingleStep.isRunning)
+            if (!MovePlayerSingleStep.isRunning && Player != null)
             {
-                ClassObjects.Gameobj.MB.StartCoroutine(MovePlayerSingleStep.Routine(ClassObjects.Gameobj.Players[1, 0]));
+                ClassObjects.Gameobj.MB.StartCoroutine(MovePlayerSingleStep.Routine(Player));
                 currStep++;
             }
 
             yield return null;
         }
 
+        Player = null;
         ClassObjects.Gameobj.MB.StartCoroutine(RollDice.Routine());
+    }
 
+    public static void PlayerToMove(GameObject player)
+    {
+        if (Player != null)
+            return;
+
+        Player = player;
+        // Player = EventSystem.current.currentSelectedGameObject;
+        // Player = ClassObjects.Gameobj.Players[0,0];
     }
 
 }
---
+++ Assets/Scripts/PlayerClicked.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class PlayerClicked : MonoBehaviour
+{
+    // Callback signature for on PlayerClicked
+    public delegate void InformMoveScriptCallback(GameObject player);
+    
+    // Event Declaration
+    public event InformMoveScriptCallback InformMoveScript;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        InformMoveScript += MovePlayer.PlayerToMove;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnMouseDown()
+    {
+        InformMoveScript(this.gameObject);
+    }
+}
